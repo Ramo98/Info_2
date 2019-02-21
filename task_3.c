05335e493b6b61822333d3831fd27e189272aeb1
Ramo98 <ramonius98@hotmail.com>
2019-02-21 23:04:01 +0100

    task 3 done

---
+++ task_3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+bool is_Perfect_Square(int x){
+    int s = (int)sqrt(x);
+    return (s*s == x);
+}
+
+bool fib(int n){
+    if(is_Perfect_Square(5 * n * n + 4) || is_Perfect_Square(5 * n * n - 4)){
+        printf("%d is a fib\n", n);
+        return true;
+    } else {
+        printf("%d is not a fib\n", n);
+
+        return false;
+    }
+}
+
+int main(){
+    fib(56);
+    fib(55);
+}
+
