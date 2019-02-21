dbfb8a666b6c631bb0e62a0b213137a05e821b83
Ramo98 <ramonius98@hotmail.com>
2019-02-21 10:22:25 +0100

    task 2 done

---
+++ task_2.c
@@ -0,0 +1,56 @@
+#include <memory.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+bool check(char array[], int x, int y, int len){
+    bool bol;
+    for(int i = 0; i < len; i++){
+        for(int j = 0; j < len; j++){
+            if(x == array[i]){
+                bol = false;
+            }
+        }
+    }
+    for(int i = 0; i < len; i++){
+        if(y == array[i]){
+            bol = false;
+        }
+    }
+    return bol;
+}
+
+void xy() {
+    int digit, x, y, len;
+
+    printf("How many numbers do you want to input?");
+    scanf("%d", &len);
+    int input[len];
+    for (int i = 0; i < len; i++) {
+        printf("Input next digit:");
+        scanf("%d", &digit);
+        input[i] = digit;
+    }
+    while(true) {
+        printf("Choose x:");
+        scanf("%d", &x);
+        printf("Choose y:");
+        scanf("%d", &y);
+        if (check(input, x, y, len) == false){
+            break;
+        }
+    }
+
+    printf("Pairs:");
+    for(int j = 0; j < len; j++){
+        //    numbers that are greater than x and less than y
+        if(input[j] > x && input[j] < y){
+            printf("(%d,%d) ", input[j], j);
+        }
+    }
+
+}
+
+int main(){
+    xy();
+}
+
